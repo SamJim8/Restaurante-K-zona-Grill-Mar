e3f60ee5fc119b6c969bbf7635cb8bd5220bd335
SamJim8 <sam.jimenez281098@gmail.com>
2025-09-27 00:24:24 -0500

    open shopping modal

--- script-shopping-car.js
+++ script-shopping-car.js
@@ -1,3 +1,15 @@
-const funcionCompleja = () => {
-    console.log("HOLA")
+const openModalPrice= ()=>{
+    const carContainer=document.getElementById("shopping-modal");
+    carContainer.style.display="block";
+    console.log(carContainer)
 }
+const funcionCompleja = () => {
+    openModalPrice();
+    // openModalPrice(); abrir carrito
+    
+    // addProduct(); añadir elemento
+    //  sumar y eliminar productos dentro del carrito
+    // que salga el precio total de los productos
+    // un input donde se coloque el nombre y que este sea conjuntamente enviado al whats con el pedido
+}
+
